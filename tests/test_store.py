d54a4039b4a4862a1ea550dc84c87a4c61790030
mnghero <mnghero829@gmail.com>
2025-07-09 13:53:27 +0300

    Добавил тест "Автоматизация раздела Store"

---
+++ tests/test_store.py
@@ -0,0 +1,67 @@
+import allure
+import requests
+
+BASE_URL='http://5.181.109.28:9090/api/v3'
+
+@allure.feature("Store")
+class TestStore:
+    @allure.title("Размещение заказа")
+    def test_add_order(self):
+        with allure.step("Отправка запроса на размещение заказа"):
+            payload = {"id": 1, "petId": 1, "quantity": 1, "status": "placed", "complete": True}
+            response = requests.post(url=f"{BASE_URL}/store/order", json=payload)
+            response_json = response.json()
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка данных заказа в ответе"):
+            assert response_json['id'] == payload['id'], "id заказа не совпадает с ожидаемым"
+            assert response_json['petId'] == payload['petId'], "petId заказа не совпадает с ожидаемым"
+            assert response_json['quantity'] == payload['quantity'], "quantity заказа не совпадает с ожидаемым"
+            assert response_json['status'] == payload['status'], "status заказа не совпадает с ожидаемым"
+            assert response_json['complete'] == payload['complete'], "complete заказа не совпадает с ожидаемым"
+
+    @allure.title("Получение информации о заказе по ID")
+    def test_get_order_by_id(self):
+        with allure.step("Отправка запроса на получение информации о заказе по ID"):
+            response = requests.get(f"{BASE_URL}/store/order/1")
+
+        with allure.step("Проверка статуса ответа и ID заказа"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            assert response.json()["id"] == 1
+
+    @allure.title("Удаление заказа по ID")
+    def test_delete_order_by_id(self, create_pet):
+        with allure.step("Удаление информации о заказе по ID"):
+            response = requests.delete(f"{BASE_URL}/store/order/1")
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Отправка запроса на получение информации о заказе по ID"):
+            response = requests.get(f"{BASE_URL}/store/order/1")
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+    @allure.title("Попытка получить информацию о несуществующем заказе")
+    def test_get_nonexistent_order(self):
+        with allure.step("Отправка запроса на получение информации о несуществующем заказе"):
+            response = requests.get(url=f"{BASE_URL}/store/order/9999")
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+    @allure.title("Получение инвентаря магазина")
+    def test_get_inventory(self):
+        with allure.step("Отправка запроса на получение информации об инвентаре магазина"):
+            response = requests.get(f"{BASE_URL}/store/inventory")
+            response_json = response.json()
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка данных инвентаря"):
+            assert response_json == {"approved":7,"delivered":50}
+
